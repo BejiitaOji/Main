b4869b815200f7b4a86fab7db4803b77453618db
Ilya Ezhov <bah6ntowl@gmail.com>
2023-08-13 01:39:39 +0300

    Добавлено исключение при вводе более двух операндов

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,45 @@ public class Main {
     }
     public static String calc(String string) throws Exception {
         String[] result = string.split(" ");
-        if (RealRoman(result[0],result[2])){
-            return Convertor.Convert(result[0],result[2],result[1]);
-        }
+
         try {
-            int a = Integer.parseInt(result[0]);
-            int b = Integer.parseInt(result[2]);
-            if (a>10 || a<1 || b>10 || b<1){
-                throw new Exception("Числа вне диапазона");
+            if (checkOperands(string)){
+                if (RealRoman(result[0],result[2])){
+                    return Convertor.Convert(result[0],result[2],result[1]);
+                }
+
+                int a = Integer.parseInt(result[0]);
+                int b = Integer.parseInt(result[2]);
+                if (a>10 || a<1 || b>10 || b<1){
+                    throw new Exception("Числа вне диапазона");
+                }
+                return Convertor.Convert(a,b, result[1]);
+
+            } else {
+                throw new Exception("Формат математической операции не удовлетворяет заданию ");
             }
-            return Convertor.Convert(a,b, result[1]);
-        } catch(Exception exception) {
+        } catch (Exception exception) {
             throw exception;
         }
 
     }
+
+    private static boolean checkOperands(String s){
+        String[] operators = {"+", "*", "/", "-"};
+
+        for (String element : operators){
+            //System.out.println(counterSymbols(s, element));
+            if (counterSymbols(s, element) >= 2){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int counterSymbols(String s, String target) {
+        return (s.length() - s.replace(target, "").length()) / target.length();
+    }
+
     private static boolean RealRoman(String s, String s1) {
         String[] ROMAN_NUM = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         int k =0;
